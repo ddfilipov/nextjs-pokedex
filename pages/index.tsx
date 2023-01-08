8d0acdd8563959fc8c90b9b3fff84c89f5f1dd60
ddfilipov <denisfilipov12+3@gmail.com>
2023-01-08 18:13:48 +0100

    results showing now

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,23 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
+import { GetStaticProps } from "next";
+import axios from "axios";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function Home({ results }: any) {
     return (
         <>
             <h1>Pokédex</h1>
-            <p>asd</p>
+            {JSON.stringify(results)}
         </>
     );
 }
+
+export const getStaticProps: GetStaticProps = async (context) => {
+    const res = await axios.get("https://pokeapi.co/api/v2/pokemon");
+    const { results }: any = await res.data;
+    
+    return { props: { results: results } };
+};
