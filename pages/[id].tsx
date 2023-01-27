6ede007954d4b6b2b143234fc3f470839740d337
ddfilipov <denisfilipov12+3@gmail.com>
2023-01-27 22:58:04 +0100

    unused stuff out

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -4,36 +4,6 @@ import styled from "styled-components";
 import { baseUrl } from ".";
 import { Pokemon, IGetPokemon, IPokemonBasicData } from "../types/types";
 
-const Wrapper = styled.div`
-    display: grid;
-    grid-template-rows: auto 25px;
-    justify-items: center;
-    border: 1px solid black;
-    padding: 5px;
-    min-height: 260px;
-    min-width: 200px;
-    place-content: center;
-`;
-
-const TypeContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    place-content: center;
-    column-gap: 10px;
-    width: 100%;
-    align-items: center;
-`;
-
-const TopCard = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-items: center;
-    place-content: center;
-    column-gap: 10px;
-    width: 100%;
-    align-items: center;
-`;
-
 export const PokemonData = ({ name, id }: IPokemonBasicData) => {
     return (
         <div>
@@ -60,7 +30,8 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
     const res = await axios.get(baseUrl + "/" + params?.id);
-    // console.log("a ver ese res.data:", res.data);
+    console.log("a ver ese res.data:", res.data);
+
     const { name }: Pokemon = await res.data;
 
     return { props: { name } };
