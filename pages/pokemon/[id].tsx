98c0b9bb6f9c652f15d7aa5e1b566da4956aa0a1
ddfilipov <denisfilipov12+3@gmail.com>
2023-01-29 18:20:02 +0100

    passing tyoes correctly now

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -3,11 +3,13 @@ import { GetStaticPaths, GetStaticProps } from "next";
 import { baseUrl } from "..";
 import { Pokemon, IGetPokemon, IPokemonBasicData } from "../../types/types";
 
-export const PokemonData = ({ name, id }: IPokemonBasicData) => {
+export const PokemonData = ({ name, id, types, src }: IPokemonBasicData) => {
     return (
         <div>
             <h1>POKEMON NAME: {name}</h1>
             <h1>POKEMON id: {id}</h1>
+            <h1>POKEMON types: {types}</h1>
+            <h1>POKEMON src: {src}</h1>
             <p>POKEMON DATA</p>
         </div>
     );
@@ -32,8 +34,15 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     console.log("checking params.id:", params?.id);
     const res = await axios.get(baseUrl + "/" + params?.id);
     // console.log("a ver ese res.data:", res.data);
+    console.log("res.data.name:", res.data.name);
+    console.log("res.data.src:", res.data.src);
+    console.log("res.data.id:", res.data.id);
+    console.log("res.data.types:", res.data.types);
+    const types = await res.data.types.map((type: any) => {
+        return type.type.name;
+    });
 
-    const { name, id }: IPokemonBasicData = await res.data;
+    const { name, id, src }: IPokemonBasicData = await res.data;
 
-    return { props: { name, id} };
+    return { props: { name, id, types } };
 };
