a22933e79557f1e3707b985a56814eb24da02b91
ddfilipov <denisfilipov12+3@gmail.com>
2023-02-02 18:41:03 +0100

    more styles to PokemonData

--- src/components/PokemonData.tsx
+++ src/components/PokemonData.tsx
@@ -5,15 +5,18 @@ import imageLoader from "../imageLoader";
 import { IPokemonBasicData } from "../types/types";
 
 const Wrapper = styled.div`
-    
-`
+    border: 1px solid white;
+    padding: 10px;
+    background-color: var(--dark-color);
+    color: white;
+`;
 
 export const PokemonData: FC<IPokemonBasicData> = ({ name, id, types, src }) => {
     return (
         <Wrapper>
-            <h1>POKEMON NAME: {name}</h1>
-            <h1>POKEMON id: {id}</h1>
-            <h1>POKEMON types: {types}</h1>
+            <h1>{name}</h1>
+            <h1>{id}</h1>
+            <h1>{types}</h1>
             <Image src={src} alt={name} width={200} height={200} loader={imageLoader} unoptimized />
             <p>POKEMON DATA</p>
         </Wrapper>
