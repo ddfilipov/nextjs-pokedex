4a3ed5424f64b164b6d688934cd6d9a45340a408
ddfilipov <denisfilipov12+3@gmail.com>
2023-02-04 18:19:21 +0100

    noooow we talking

--- src/components/PokemonData.tsx
+++ src/components/PokemonData.tsx
@@ -7,7 +7,7 @@ import { IPokemonBasicData } from "../types/types";
 
 const Wrapper = styled.div`
     display: grid;
-    grid-template-columns: 2fr 1fr 1fr;
+    grid-template-columns: 2fr 1fr;
     grid-template-rows: auto auto;
     gap: 10px;
     max-width: 1200px;
@@ -34,7 +34,7 @@ const MovesWrapper = styled.div`
     display: flex;
     grid-area: 2 / 1 / 2 / 3;
     background-color: green;
-    width: 500px;
+    width: auto;
     height: 500px;
 `;
 
