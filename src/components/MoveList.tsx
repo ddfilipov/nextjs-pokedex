c2757e7927a25811b2ad6181ee387d30a0d6c720
ddfilipov <denisfilipov12+3@gmail.com>
2023-02-05 15:49:53 +0100

    added some code in MoveList

--- src/components/MoveList.tsx
+++ src/components/MoveList.tsx
@@ -1,10 +1,21 @@
 import { FC } from "react";
+import styled from "styled-components";
 
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
 
-export const MoveList : FC = () => {
+export const MoveList: FC = () => {
     return (
-        <>
-        </>
+        <Container>
+            <ul>
+                MOVES
+                <li>Move 1</li>
+                <li>Move 2</li>
+                <li>Move 3</li>
+            </ul>
+        </Container>
     );
 };
 export default MoveList;
