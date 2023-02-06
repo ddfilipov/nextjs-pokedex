9d391fa4aea1e56e1cf6f520ba75b700ec9afd47
ddfilipov <denisfilipov12+3@gmail.com>
2023-02-06 18:42:05 +0100

    reset.css added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import GlobalStyle, { MainAreaContainer } from "../styles/GlobalStyle";
+import "../assets/css/reset.css"
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
