04f3c7680f5c17c0e1f1650ad5932df46be3d079
ddfilipov <denisfilipov12+3@gmail.com>
2023-02-19 08:23:05 +0100

    unused comments out

--- src/components/PokemonData.tsx
+++ src/components/PokemonData.tsx
@@ -49,8 +49,6 @@ export const PokemonData: FC<IPokemonExtendedData> = ({ name, id, types, src, mo
     const router = useRouter();
 
     useEffect(() => {
-        // console.log("hola PokemonData, a ver esos moves:", moves);
-        // console.log("hola PokemonData, a ver esos id:", id);
         // console.log("hola PokemonData, a ver esos stats:", stats);
         console.log("hola PokemonData, a ver esos stats2:", stats2);
         // console.log("hola PokemonData, a ver esos stats2:", Object.keys(stats2));
