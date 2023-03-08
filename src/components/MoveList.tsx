db289ff94d3434c69438bbb1b811e0309abc9439
ddfilipov <denisfilipov12+3@gmail.com>
2023-03-08 18:24:37 +0100

    it's filtering, not like i want it to tho

--- src/components/MoveList.tsx
+++ src/components/MoveList.tsx
@@ -36,7 +36,7 @@ export const MoveList: FC<MoveListProps> = ({ movesList }) => {
         //     move.version_group_details.find((move2) => move2.version_group.name === defaultGameVersion)
         // );
         console.log(movesList);
-        const filteresMoves = movesList.filter((move) => {
+        const filteresMoves = movesList.map((move) => {
             // return move.version_group_details.find((move2) => move2.version_group.name === defaultGameVersion);
             return {
                 ...move,
